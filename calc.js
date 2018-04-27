d4d9a6c60c7b16bd90fc253151e3fbc0560d3cb6
Jesse <laethaka@gmail.com>
2018-04-27 13:48:48 -0700

    first input works

--- calc.js
+++ calc.js
@@ -1,11 +1,41 @@
 $(document).ready(function() {
 
     var firstNum = '';
+    var operator = '';
+    var secondNum = '';
+    var operatorPressed = false;
+
+
+
+    // if (!operatorPressed) { //FIRST NUMBER INPUT
+    //     $('.number').on('click', function () {
+    //         firstNum += $(this).attr('value');
+    //         $('#first-number').text(firstNum);
+    //     })
+    // } else { //SECOND NUMBER INPUT
+    //     $('.number').on('click', function () {
+    //         secondNum += $(this).attr('value');
+    //         $('#second-number').text(secondNum);
+    //     })
+    // }
 
     $('.number').on('click', function () {
-        firstNum+=$(this).attr('value');
-        $('#first-number').text(firstNum);
+        if (!operatorPressed) { //OPERATOR HASN'T BEEN HIT
+            firstNum += $(this).attr('value');
+            $('#first-number').text(firstNum);
+        } else {
+            secondNum += $(this).attr('value');
+            $('#second-number').text(secondNum);
+        }
     })
+
+    $('.operator').on('click', function() {
+        operator += $(this).attr('value');
+        $('#operator').text(operator);
+        operatorPressed = true;
+    })
+
+    $('.equal').on('click')
     
     
     
